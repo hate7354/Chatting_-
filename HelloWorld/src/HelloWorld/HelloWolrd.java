054fe1b93b5c56fd60ac5ca5ccfda101334436d5
Jin\User <hate7354@naver.com>
2021-03-22 12:20:43 +0900

    Chnage 이진웅 입니다

--- HelloWorld/src/HelloWorld/HelloWolrd.java
+++ HelloWorld/src/HelloWorld/HelloWolrd.java
@@ -4,7 +4,7 @@ public class HelloWolrd {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("테스트 입니당 마음껏 바꾸셔도 상관없습니다.이진웅 입니다");
+		System.out.println("테스트 입니당 마음껏 바꾸셔도 상관없습니다.이진웅 입니다ㅁㄴㅇ");
 
 }
 }
